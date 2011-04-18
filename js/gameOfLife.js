fd28b5335a216c17ce76e9670bc5867f7a9bdf36
Thomas Kjeldahl Nilsson <thomas@kjeldahlnilsson.net>
2011-04-18 09:07:11 +0200

    Canvas rendering pretty much done, only need actual rules of the game in place

--- js/gameOfLife.js
+++ js/gameOfLife.js
@@ -1,66 +1,91 @@
-var gameOfLife = (function() { // Module pattern
+var gameOfLife = (function() { // Module pattern	
+	var zoom = 5;
+	var ctx = null;
 	
-	// Private variables and methods
-	
-
-	
-	return {
-		// Public methods
+	return {	
 		
+		// Game state 
 		grid : null,
-		ctx : null,
 
+		// Game logic
 		initState: function(width, height){
 			this.grid = new Grid(width, height);
 		},
+				
+		randomSeededState : function(width, height) {
+			var seed = new Grid(width, height);
+			return seed.map(function(cell, x, y) {
+				var percentLikelyAlive = 10;
+				var deadOrAlive = Math.floor(Math.random() * 100);
+				if(deadOrAlive <= percentLikelyAlive){
+					return 1;
+				} else {
+					return 0;
+				}
+			});
+		},
 		
+		deadOrAlive: function(grid, x, y){
+			return 1; // A live cell is 1, a dead cell is 0
+		},
+				
+		tick: function(grid){
+			return grid.map(function(cell, x, y) {
+				return gameOfLife.deadOrAlive(grid, x ,y)
+			});
+		},
+		
+		// Canvas drawing
 		initCanvas: function(width, height){
 				var canvas = $("canvas")[0];
 				canvas.width = width;
 				canvas.height = height;
-				this.ctx = $("canvas")[0].getContext('2d');
-		},
-		
-		// A live cell is 1, a dead cell is 0
-		updatedCellState: function(grid, x, y){
-			return 1;
-		},
-		
-		
-		tick: function(grid){
-			return grid.map(function(cell, x, y) {
-				return gameOfLife.updatedCellState(grid, x ,y)
-			});
+				ctx = $("canvas")[0].getContext('2d');
 		},
 
-		// TODO blow up up grid, paint each pixel as big block
-		drawCell: function(x, y){		    
-			var id = this.ctx.getImageData(0, 0, 1, 1);
-			id.data[3] = 255; // set opacity
-			var r = Math.floor(Math.random() * 256);
+		randomRgbStyle: function(){
+			var r = 0;
 			var g = Math.floor(Math.random() * 256);
-			var b = Math.floor(Math.random() * 256);
-			id.data[0] = r;
-			id.data[1] = g;
-			id.data[2] = b;
-			
-			this.ctx.putImageData(id, x, y);
+			var b = 0;
+			return "rgb("+r+","+g+","+b+")";
+		},
+
+		drawCell: function(x, y){
+			var z = zoom;
+			ctx.fillStyle = this.randomRgbStyle();
+			ctx.fillRect (x*z, y*z, z, z);
 		},
 		
 		drawState: function(grid){
 			var that = this;
 			grid.each(function(cell, x, y) {
-				that.drawCell(x, y);
+				if (cell !== 0) {
+					that.drawCell(x, y);	
+				}
 			});
 		},
 		
+		clearCanvas: function(width, height){
+			ctx.clearRect(0, 0, width, height);
+		},
+		
 		begin: function(){
-			this.initState(200, 200);
-			this.initCanvas(200, 200);
+			var width = 150;
+			var height = 80;
+			var z = zoom;
+			this.initState(width, height);
+			this.grid = this.randomSeededState(width, height);
+			this.initCanvas(width*z, height*z);
 
-			// 10 times every second
-			this.grid = this.tick(this.grid);
-			this.drawState(this.grid);
+			var that = this;
+
+			//var fps = 5;
+			window.setInterval(function() {
+				that.grid = that.tick(that.grid);
+				that.clearCanvas(width*z, height*z);
+				that.drawState(that.grid);
+			}, 300);
+			
 		},
 		
 	};
